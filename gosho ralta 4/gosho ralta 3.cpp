2909f3bf4b40435584a4b71e5e2c94dd8985082e
GeorgiRadevGeorgiev <go6o_mamuta@abv.bg>
2016-10-28 12:45:28 +0300

    Add files via upload

--- gosho ralta 4/gosho ralta 3.cpp
+++ gosho ralta 4/gosho ralta 3.cpp
@@ -75,14 +75,14 @@ int main()
 	cout << "input number of \"*\" ";
 	cin >> n;
 	for (int i = 0; i <= n; i++) {
-		for (int j = 1 ; j <= i; j++) {
+		for (int j = 1 ; j < i; j++) {
 			cout<<'*';
 			
 		}cout << "\n";
 	}
-
+	
 	for (int i = 0; i <= n; i++) {
-		for (int j = i + 1; j <= n + 1; j++) {
+		for (int j = i; j < n; j++) {
 			cout << '*';
 
 		}cout << ' ' << endl;
